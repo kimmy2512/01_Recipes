3812b14ea951e39c65d81f859c410d92ceb58650
kimmy2512 <51727300+kimmy2512@users.noreply.github.com>
2020-02-24 13:07:08 +1300

    working program

--- 10_Recipe_Moderniser.py
+++ 10_Recipe_Moderniser.py
@@ -73,11 +73,39 @@ def get_sf():
 
     return scale_factor
 
+# Function to get (and check amount, unit and ingredient)
+def get_all_ingredients():
+    all_ingredients = []
+
+    stop = ""
+    print("Please enter ingredients one line at a time. Press 'xxx' to when you are done.")
+    while stop != "xxx":
+        # Ask user for ingredient (via not blank function)
+        get_recipe_line = not_blank("Recipe Line: ",
+                                    "This can't be blank",
+                                    "yes")
+
+        # Stop looping if exit code is typed and there are more than 2 ingredients..
+        if  get_recipe_line.lower() == "xxx" and len(all_ingredients) > 1:
+            break
+
+        elif get_recipe_line.lower() == "xxx" and len(all_ingredients) <2:
+            print("You neewd at least two ingredients in the list. "
+                  "Please add more ingredients. ")
+
+        # If exit code is not entered, add ingredient to list
+        else:
+            all_ingredients.append(get_recipe_line)
+
+    return all_ingredients
+
+
 # Main routine goes here
 
 # Set up Dictionaries
 
 # Set up list to hold 'modernised' ingredients
+modernised_recipe = []
 
 # Ask user for recipe name and check its not blank
 recipe_name = not_blank("What is the recipe name ",
@@ -91,13 +119,65 @@ source = not_blank("What is the recipe from? ",
 
 # Get serving sizes and scale factor
 scale_factor = get_sf()
-print(scale_factor)
 
-# Loop for each ingredient...
+# Get amounts, units and ingredients from user...
+full_recipe = get_all_ingredients()
 
-# Get ingredient amount
-# Get ingredient name
-# Get unit
-# Convert unit to ml
-# Convert from ml to g
+# Split each line of the recipe into amount, unit and ingredient...
+mixed_regex = "\d{1,3}\s\d{1,3}\/\d{1,3}"
+
+for recipe_line in full_recipe:
+    recipe_line = recipe_line.strip()
+
+    # Get amount...
+    if re.match(mixed_regex, recipe_line):
+
+        # Get mixed number by matching the regex
+        pre_mixed_num = re.match(mixed_regex, recipe_line)
+        mixed_num = pre_mixed_num.group()
+
+        # Replace space with a + sign...
+        amount = mixed_num.replace(" ", "+")
+        # Change the string into a decimal
+        amount = eval(amount)
+        amount = amount * scale_factor
+
+        # Get unit and ingredient...
+        compile_regex = re.compile(mixed_regex)
+        unit_ingredient = re.split(compile_regex, recipe_line)
+        unit_ingredient = (unit_ingredient[1]).strip()   # Remove extra white space from unit
+
+    else:
+        get_amount = recipe_line.split(" ", 1)   # Split line at first space
+
+        try:
+            amount = eval(get_amount[0])    # Convert amount to float if possible
+            amount = amount * scale_factor
+        except NameError:
+            amount = get_amount[0]
+            modernised_recipe.append(recipe_line)
+            continue
+
+        unit_ingredient = get_amount[1]
+
+    # Get unit and ingredient...
+    get_unit = unit_ingredient.split(" ", 1)  # Splits text at first space
+
+    unit = get_unit[0]
+    # Convert unit to ml
+
+    num_spaces = recipe_line.count(" ")
+    if num_spaces > 1:
+        ingredient = get_unit [1]
+    # Convert from ml to g
+    else:
+        modernised_recipe.append("{} {} {}".format(amount, unit, ingredient))
+        continue
+
+    modernised_recipe.append("{} {} {}".format(amount, unit, ingredient))
+
 # Put updated ingredient in list
+
+# Output ingredient list
+for item in modernised_recipe:
+    print(item)
